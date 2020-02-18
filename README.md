7668afa8974cf343086f9d585d931fb9926a7bc2
Thomas Nguyen <ngutt96@uw.edu>
2020-02-18 00:24:14 -0800

    Finished final implementation and documentation

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
 # notable-take-home
+
+## Description
+
+This is a Python project that parses an input string and transforms certain key phrases
+into an ordered list format. This project is designed to purely be functional and stateless.
+
+## Usage
+
+In the root of the project, simply run the following command in the command line.
+
+```
+python3 TranscriptTransformer.py
+```
+
+Currently, a sample string that is to be transformed is hardcoded into the program. However,
+this should be changed.
+
+## Assumptions
+
+Due to the vagueness of the spec, a few assumptions were made.
+
+1. If there is no starting phrase 'Number n' where 'n' is 'one' through 'nine',
+then the program will assume improper input and error.
+2. It is assumed that the last item in the ordered list will contain the remaining
+of the input string.
+3. The first instance of 'Number n' is assumed to be the start of the ordered list.
+Subsequent occurrences of 'Number n' will be assumed to be content for the ordered
+list items.
+4. In general, the program assumes proper input from the client. 
---
+++ TranscriptTransfomer.py
@@ -0,0 +1,30 @@
+
+import re
+
+
+# driver code
+def main():
+    transcription = 'Patient presents today with several issues. Number one BMI has increased by 10% since their last visit. Number next patient reports experiencing dizziness several times in the last two weeks. Number next patient has a persistent cough that hasn’t improved for last 4 weeks.'
+    trans = transform(transcription)
+    print(trans)
+
+# Returns a transformation of the string input that has an ordered list
+def transform(transcription):
+    start = re.search('Number (?:one|two|three|four|five|six|seven|eight|nine) ', transcription)
+    if not start:
+        raise ValueError('Input did not have a starting phrase')
+    nums_map = {'one':1, 'two':2, 'three':3, 'four':4, 'five':5, 'six':6, 'seven':7, 'eight':8, 'nine':9}
+    starting_statement = transcription[start.start():start.end()]
+    word_num = starting_statement.split(' ')[1]
+    curr_num = nums_map[word_num]
+    split_by_items = re.split(' Number (?:one|two|three|four|five|six|seven|eight|nine|next) ', transcription)
+    index = 1
+    while index < len(split_by_items):
+        split_by_items[index] = '\n' + str(curr_num) + '. ' + split_by_items[index][0].capitalize() + split_by_items[index][1:]
+        index += 1
+        curr_num += 1
+    return ''.join(split_by_items)
+
+# python module name guard
+if __name__ == '__main__':
+    main()
